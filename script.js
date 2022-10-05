5674d9482ab4b61eeae238ea8d83c46a0db18adf
scinovus <114958661+scinovus@users.noreply.github.com>
2022-10-05 08:38:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -18,9 +18,29 @@ function onYouTubeIframeAPIReady() {
 
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+document.head = document.head || document.getElementsByTagName('head')[0];
+
+const changeFavicon = link => {
+  let $favicon = document.querySelector('link[rel="icon"]')
+  // If a <link rel="icon"> element already exists,
+  // change its href to the given link.
+  if ($favicon !== null) {
+    $favicon.href = link
+  // Otherwise, create a new element and append it to <head>.
+  } else {
+    $favicon = document.createElement("link")
+    $favicon.rel = "icon"
+    $favicon.href = link
+    document.head.appendChild($favicon)
+  }
+}
+
 function hide() {
+  player.unMute();
   setTimeout(function () {
     // delay for video to start
+    document.title = "Get Rickrolled!";
+    changeFavicon('https://cdn.glitch.global/e53a2255-5e5e-4df1-a15d-47250e916490/rickastley.ico?v=1664885959721');
     const overlay = document.getElementById("permission");
     overlay.style.display = "none";
   }, 500);
